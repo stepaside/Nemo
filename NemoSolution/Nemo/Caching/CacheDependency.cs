e2ecf8c46f6113c6f548ea56a3f12bc92ff8d57b
Max <Stepanskiy>
2013-02-07 11:11:09 -0500

    Added cache dependency classes

---
+++ NemoSolution/Nemo/Caching/CacheDependency.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nemo.Caching
+{
+    public class CacheDependency
+    {
+        public Type DependentType { get; set; }
+        public string DependentProperty { get; set; }
+        public string ValueProperty { get; set; }
+    }
+}
