6a42bf5d3a2e16cc7943e466b8bf49839ef8569f
Max Stepanskiy <mstepanskiy@asicentral.com>
2013-09-14 18:58:54 -0400

    Fixed ExtractParameters

--- NemoSolution/Nemo/ParamList.cs
+++ NemoSolution/Nemo/ParamList.cs
@@ -7,8 +7,6 @@ namespace Nemo
 {
     public class ParamList : List<Expression<Func<object, object>>>
     {
-        private static ConcurrentDictionary<Tuple<Type, string, string, Type>, Func<object, object>> _parameterCache = new ConcurrentDictionary<Tuple<Type, string, string, Type>, Func<object, object>>();
-
         internal Param[] ExtractParameters(Type objectType, string operation)
         {
             var result = new Param[this.Count];
@@ -16,25 +14,9 @@ namespace Nemo
             {
                 var expression = this[i];
                 var parameterName = expression.Parameters[0].Name;
-                var returnType = expression.Body.Type;
-                var key = Tuple.Create(objectType, operation, parameterName, returnType);
-                Func<Tuple<Type, string, string, Type>, Func<object, object>> valueFactory = t => expression.Compile();
-                var func = _parameterCache.GetOrAdd(key, valueFactory);
-
-                var parameter = new Param { Name = parameterName };
-
-                if (returnType == typeof(Param))
-                {
-                    parameter = (Param)func(null);
-                    if (parameter.Name == null)
-                    {
-                        parameter.Name = parameterName;
-                    }
-                }
-                else
-                {
-                    parameter.Value = func(null);
-                }
+                var parameterValue = expression.Body is ConstantExpression ? ((ConstantExpression)expression.Body).Value : expression.Compile()(null);
+                
+                var parameter = new Param { Name = parameterName, Value = parameterValue };
 
                 result[i] = parameter;
             }
