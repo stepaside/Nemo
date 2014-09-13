28effc155f5f1c2e53c878f609cd9eff22047920
Max Stepanskiy <mstepanskiy@asicentral.com>
2014-09-12 23:52:43 -0400

    Updated provider name detection

--- NemoSolution/Nemo/Data/DbFactory.cs
+++ NemoSolution/Nemo/Data/DbFactory.cs
@@ -64,59 +64,73 @@ namespace Nemo.Data
         {
             if (connectionString == null) return null;
 
-			var builder = new DbConnectionStringBuilder { ConnectionString = connectionString };
+            var builder = new DbConnectionStringBuilder { ConnectionString = connectionString };
 
-			object providerValue;
-			if (builder.TryGetValue("provider", out providerValue))
-			{
-				return providerValue.ToString();
-			}
+            object providerValue;
+            if (builder.TryGetValue("provider", out providerValue))
+            {
+                return providerValue.ToString();
+            }
 
-			var persistSecurityInfo = false;
-			object persistSecurityInfoValue;
-			if (builder.TryGetValue("persist security info", out persistSecurityInfoValue))
-			{
-				persistSecurityInfo = Convert.ToBoolean(persistSecurityInfoValue);
-			}
+            var persistSecurityInfo = false;
+            object persistSecurityInfoValue;
+            if (builder.TryGetValue("persist security info", out persistSecurityInfoValue))
+            {
+                persistSecurityInfo = Convert.ToBoolean(persistSecurityInfoValue);
+            }
 
             var lostPassword = !persistSecurityInfo && !builder.ContainsKey("pwd") && !builder.ContainsKey("password");
 
             if (!lostPassword)
-			{
-				for (var i = 0; i < ConfigurationManager.ConnectionStrings.Count; i++)
-				{
-					var config = ConfigurationManager.ConnectionStrings [i];
-				    if (string.Equals(config.ConnectionString, connectionString, StringComparison.OrdinalIgnoreCase))
-				    {
-				        return config.ProviderName;
-				    }
-				}
-			} 
-			else
-			{
-				for (var i = 0; i < ConfigurationManager.ConnectionStrings.Count; i++)
-				{
-					var config = ConfigurationManager.ConnectionStrings[i];
+            {
+                for (var i = 0; i < ConfigurationManager.ConnectionStrings.Count; i++)
+                {
+                    var config = ConfigurationManager.ConnectionStrings[i];
+                    if (string.Equals(config.ConnectionString, connectionString, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return config.ProviderName;
+                    }
+                }
+            }
+            else
+            {
+                object uid;
+                if (builder.TryGetValue("uid", out uid))
+                {
+                    builder.Remove("uid");
+                    builder["user id"] = uid;
+                }
+
+                for (var i = 0; i < ConfigurationManager.ConnectionStrings.Count; i++)
+                {
+                    var config = ConfigurationManager.ConnectionStrings[i];
 
                     var otherBuilder = new DbConnectionStringBuilder { ConnectionString = config.ConnectionString };
                     otherBuilder.Remove("pwd");
                     otherBuilder.Remove("password");
 
+                    object otherUid;
+                    if (otherBuilder.TryGetValue("uid", out otherUid))
+                    {
+                        otherBuilder.Remove("uid");
+                        otherBuilder["user id"] = otherUid;
+                    }
+
                     if (otherBuilder.Count != builder.Count) continue;
 
-				    var equivalenCount = builder.Cast<KeyValuePair<string, object>>().Select(p =>
-				    {
-				        object value;
-				        return otherBuilder.TryGetValue(p.Key, out value) && string.Equals(Convert.ToString(value), Convert.ToString(p.Value), StringComparison.OrdinalIgnoreCase) ? 1 : 0;
-				    }).Sum();
+                    var equivalenCount = builder.Cast<KeyValuePair<string, object>>().Select(p =>
+                    {
+                        object value;
+                        return otherBuilder.TryGetValue(p.Key, out value) && string.Equals(Convert.ToString(value), Convert.ToString(p.Value), StringComparison.OrdinalIgnoreCase) ? 1 : 0;
+                    }).Sum();
 
                     if (equivalenCount == builder.Count)
                     {
                         return config.ProviderName;
                     }
-				}
-			}
-           
+                }
+            }
+
             return null;
         }
 
