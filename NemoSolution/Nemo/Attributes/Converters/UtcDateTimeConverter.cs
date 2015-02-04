688c9697dc19095c6698bdb922fcb35c731a3db1
Max Stepanskiy <mstepanskiy@asicentral.com>
2015-02-03 22:26:35 -0500

    Added UtcDateTimeConverter

---
+++ NemoSolution/Nemo/Attributes/Converters/UtcDateTimeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nemo.Attributes.Converters
+{
+    public class UtcDateTimeConverter : ITypeConverter<object, DateTime>
+    {
+        #region ITypeConverter<object, DateTime> Members
+
+        DateTime ITypeConverter<object, DateTime>.ConvertForward(object from)
+        {
+            return DateTime.SpecifyKind((DateTime)from, DateTimeKind.Utc);
+        }
+
+        object ITypeConverter<object, DateTime>.ConvertBackward(DateTime to)
+        {
+            return to.Kind == DateTimeKind.Utc ? to : to.ToUniversalTime();
+        }
+
+        #endregion
+    }
+}
