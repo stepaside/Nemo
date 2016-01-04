c3a8917b0eb4790aa009d846b280866d508bbe36
Max Stepanskiy <mstepanskiy@asicentral.com>
2016-01-04 17:28:02 -0500

    Added COMB GUID generator

---
+++ NemoSolution/Nemo/Id/CombGuidGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nemo.Utilities;
+
+namespace Nemo.Id
+{
+    public class CombGuidGenerator : IIdGenerator
+    {
+        public object Generate()
+        {
+            return CombGuid.Generate();
+        }
+    }
+}
---
+++ NemoSolution/Nemo/Utilities/CombGuid.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nemo.Utilities
+{
+    public static class CombGuid
+    {
+        public static readonly Guid Empty = Guid.Parse("00000000-0000-0000-0000-000000000000");
+
+        private static readonly Func<Guid> GeneratorCore = () =>
+        {
+            var guidArray = Guid.NewGuid().ToByteArray();
+
+            var baseDate = new DateTime(1900, 1, 1);
+            var now = DateTime.Now;
+
+            // Get the days and milliseconds which will be used to build the byte string 
+            var days = new TimeSpan(now.Ticks - baseDate.Ticks);
+            var msecs = now.TimeOfDay;
+
+            // Convert to a byte array 
+            // Note that SQL Server is accurate to 1/300th of a millisecond so we divide by 3.333333 
+            byte[] daysArray = BitConverter.GetBytes(days.Days);
+            byte[] msecsArray = BitConverter.GetBytes((long)(msecs.TotalMilliseconds / 3.333333));
+
+            // Reverse the bytes to match SQL Servers ordering 
+            Array.Reverse(daysArray);
+            Array.Reverse(msecsArray);
+
+            // Copy the bytes into the guid 
+            Array.Copy(daysArray, daysArray.Length - 2, guidArray, guidArray.Length - 6, 2);
+            Array.Copy(msecsArray, msecsArray.Length - 4, guidArray, guidArray.Length - 4, 4);
+
+            return new Guid(guidArray);
+        };
+
+        private static Func<Guid> _generator = GeneratorCore;
+
+        public static Guid Generate()
+        {
+            return _generator();
+        }
+
+        public static void Reset()
+        {
+            _generator = GeneratorCore;
+        }
+
+        public static IDisposable Stub(Guid value)
+        {
+            _generator = () => value;
+            return new DisposableAction(Reset);
+        }
+
+        private class DisposableAction : IDisposable
+        {
+            private readonly Action _action;
+
+            public DisposableAction(Action action)
+            {
+                _action = action;
+            }
+
+            public void Dispose()
+            {
+                _action();
+            }
+        }
+    }
+}
