0363f48ccb8310b140eb5bdbafcaf2c1429f8007
Max Stepanskiy <mstepanskiy@asicentral.com>
2021-02-12 11:03:04 -0500

    Added extension methods to configure nemo in .net core applications

---
+++ src/Nemo/Configuration/ConfigurationExtensions.cs
@@ -0,0 +1,43 @@
+﻿#if NETSTANDARD
+using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Nemo.Configuration
+{
+    public static class ConfigurationExtensions
+    {
+        public static IServiceCollection AddNemo(this IServiceCollection services, Action<IConfiguration> builder = null)
+        {
+            services.AddSingleton(new NemoConfigurationBuilder(builder));
+            services.AddSingleton<NemoConfigurationService>();
+            return services;
+        }
+    }
+
+    public class NemoConfigurationService
+    {
+        public NemoConfigurationService(Microsoft.Extensions.Configuration.IConfigurationRoot configuration, NemoConfigurationBuilder builder)
+        {
+            var config = ConfigurationFactory.Configure();
+            builder?.Build(config);
+            config.SetSystemConfiguration(configuration);
+        }
+    }
+
+    public class NemoConfigurationBuilder
+    {
+        private readonly Action<IConfiguration> _builder;
+
+        public NemoConfigurationBuilder(Action<IConfiguration> builder)
+        {
+            _builder = builder;
+        }
+
+        public void Build(IConfiguration configuration)
+        {
+            _builder?.Invoke(configuration);
+
+        }
+    }
+}
+#endif
